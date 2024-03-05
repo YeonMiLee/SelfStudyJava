19b521a7631fdcb701d05ed870f361e946c8d00a
YeonMiLee <160806889+YeonMiLee@users.noreply.github.com>
2024-03-05 10:42:42 +0900

    Add files via upload

---
+++ chap10/ClassCastExceptionEx.java
@@ -0,0 +1,23 @@
+package sec01.exam;
+
+public class ClassCastExceptionEx {
+	public static void main(String[] args) {
+		Dog dog = new Dog();
+		changeDog(dog);
+		
+		Cat cat = new Cat();
+		changeDog(cat);		
+	}
+	
+	
+	public static void changeDog(Animal animal) {
+//		if(animal instanceOf Dog) {
+		Dog dog = (Dog) animal;			//ClassCastException 발생 가능
+		}
+//	}
+
+}
+
+class Animal {}
+class Dog extends Animal {}
+class Cat extends Animal {}
